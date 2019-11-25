d356d0fd02b58b4fbd0b3f6d344e1106a0915605
Jason Main <jason_main@abercrombie.com>
2019-11-25 18:10:31 -0500

    implemented Main class to run application; implemented corresponding tests

---
+++ src/main/java/com/jmain/primegen/Main.java
@@ -0,0 +1,41 @@
+package com.jmain.primegen;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int range1 = Integer.parseInt(args[0]);
+            int range2 = Integer.parseInt(args[1]);
+            int testIterations = 1;
+
+            if (range2 >= range1)
+                testIterations = range2 / 4;
+            else
+                testIterations = range1 / 4;
+
+            if (testIterations <= 0)
+                testIterations = 1;
+
+            PrimeNumberGenerator primeGen = new PrimeNumberGeneratorImpl(testIterations);
+            List<Integer> primes = primeGen.generate(range1, range2);
+            if (primes.size() == 0)
+                System.out.println("There are no prime numbers in the specified range.");
+            else {
+                System.out.println("There are " + primes.size() + " prime numbers in the specified range.");
+                System.out.print("The prime numbers in the specified range are: ");
+                for (int i = 0; i < primes.size(); i++) {
+                    if (i < primes.size() - 1) {
+                        System.out.print(primes.get(i) + ", ");
+                    } else {
+                        System.out.print(primes.get(i));
+                    }
+                }
+                System.out.println();
+            }
+        }
+        catch (NumberFormatException e) {
+            System.out.println("usage: please only provide integer values as arguments");
+        }
+    }
+}
---
+++ src/test/java/com/jmain/primegen/MainTest.java
@@ -0,0 +1,51 @@
+package com.jmain.primegen;
+
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testMain() {
+        Main.main(new String[]{"7900", "7920"});
+
+        assertEquals("There are 3 prime numbers in the specified range.\nThe prime numbers in the specified range are: 7901, 7907, 7919\n", outContent.toString());
+    }
+
+    @Test
+    void testMainInverse() {
+        Main.main(new String[]{"7920", "7900"});
+
+        assertEquals("There are 3 prime numbers in the specified range.\nThe prime numbers in the specified range are: 7901, 7907, 7919\n", outContent.toString());
+    }
+
+    @Test
+    void testMainNumberFormatException() {
+        Main.main(new String[]{"one", "thirty"});
+
+        assertEquals("usage: please only provide integer values as arguments\n", outContent.toString());
+    }
+
+    @Test
+    void testMainNegativeRange() {
+        Main.main(new String[]{"-100", "-10"});
+
+        assertEquals("There are no prime numbers in the specified range.\n", outContent.toString());
+    }
+}
