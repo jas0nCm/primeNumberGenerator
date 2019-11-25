f8fae5c15d324c34c1c16dde8aad285e87460ef4
Jason Main <jason_main@abercrombie.com>
2019-11-25 18:09:34 -0500

    implemented generate and corresponding tests

--- src/main/java/com/jmain/primegen/PrimeNumberGeneratorImpl.java
+++ src/main/java/com/jmain/primegen/PrimeNumberGeneratorImpl.java
@@ -1,11 +1,31 @@
 package com.jmain.primegen;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeNumberGeneratorImpl implements PrimeNumberGenerator {
+    int iterations;
+
+    public PrimeNumberGeneratorImpl(int iterations) {
+        this.iterations = iterations;
+    }
     @Override
     public List<Integer> generate(int startingValue, int endingValue) {
-        return null;
+        List<Integer> primesList = new ArrayList<>();
+
+        if (endingValue < startingValue) {
+            int temp = endingValue;
+            endingValue = startingValue;
+            startingValue = temp;
+        }
+
+        for (int i = startingValue; i <= endingValue; i++) {
+            if (isPrime(i)) {
+                primesList.add(i);
+            }
+        }
+
+        return  primesList;
     }
 
     @Override
--- src/test/java/com/jmain/primegen/PrimeNumberGeneratorImplTest.java
+++ src/test/java/com/jmain/primegen/PrimeNumberGeneratorImplTest.java
@@ -2,17 +2,50 @@ package com.jmain.primegen;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeNumberGeneratorImplTest {
 
     @Test
     void generate() {
+        PrimeNumberGenerator primeGen = new PrimeNumberGeneratorImpl(5);
+        List<Integer> primesList = primeGen.generate(7900, 7920);
+
+        List<Integer> expectedPrimesList = new ArrayList<>();
+        expectedPrimesList.add(Integer.valueOf(7901));
+        expectedPrimesList.add(Integer.valueOf(7907));
+        expectedPrimesList.add(Integer.valueOf(7919));
+
+        assertEquals(expectedPrimesList, primesList);
+
+        expectedPrimesList.add(Integer.valueOf(7917));
+
+        assertNotEquals(expectedPrimesList, primesList);
+    }
+
+    @Test
+    void generateReversRange() {
+        PrimeNumberGenerator primeGen = new PrimeNumberGeneratorImpl(5);
+        List<Integer> primesList = primeGen.generate(7920, 7900);
+
+        List<Integer> expectedPrimesList = new ArrayList<>();
+        expectedPrimesList.add(Integer.valueOf(7901));
+        expectedPrimesList.add(Integer.valueOf(7907));
+        expectedPrimesList.add(Integer.valueOf(7919));
+
+        assertEquals(expectedPrimesList, primesList);
+
+        expectedPrimesList.add(Integer.valueOf(7917));
+
+        assertNotEquals(expectedPrimesList, primesList);
     }
 
     @Test
     void isPrime_trivialCases() {
-        PrimeNumberGenerator primeGen= new PrimeNumberGeneratorImpl();
+        PrimeNumberGenerator primeGen = new PrimeNumberGeneratorImpl(5);
         assertFalse(primeGen.isPrime(-5));
         assertFalse(primeGen.isPrime(0));
         assertFalse(primeGen.isPrime(1));
